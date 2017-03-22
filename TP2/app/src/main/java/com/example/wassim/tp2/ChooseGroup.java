de3de2665ee463f48ef13c927337feece7f18bc3
loforb <louis-philippe.fortin-houde@polymtl.ca>
2017-03-22 17:01:13 -0400

    ui now call the backend group

--- TP2/app/src/main/java/com/example/wassim/tp2/ChooseGroup.java
+++ TP2/app/src/main/java/com/example/wassim/tp2/ChooseGroup.java
@@ -1,11 +1,13 @@
 package com.example.wassim.tp2;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.example.wassim.tp2.DataStructures.Group;
 
 /**
  * Created by Wassim on 28/02/2017.
@@ -42,7 +44,7 @@ public class ChooseGroup extends AppCompatActivity {
 
 
                 EditText text = (EditText)findViewById(R.id.textGroup);
-
+                Group.getOrCreateGroup(text.getText().toString(),(Activity) getApplicationContext());
                 Intent i = new Intent(getBaseContext(), Group.class);
                 startActivity(i);
 
@@ -51,6 +53,7 @@ public class ChooseGroup extends AppCompatActivity {
                 startActivity(i2);
 
                 //TODO: backend group name
+
             }
         });
     }

dece5b28245853755008b013048082beaeee0323
Wassim <wassim.nasrallah.36@gmail.com>
2017-03-23 20:07:31 -0400

    Merge branch 'dev' of https://github.com/wassimNasrallah/INF8405-TP2 into dev
    # Conflicts:
    #	TP2/app/src/main/java/com/example/wassim/tp2/DataStructures/Settings.java

--- TP2/app/src/main/java/com/example/wassim/tp2/Map.java
+++ TP2/app/src/main/java/com/example/wassim/tp2/Map.java
@@ -1,5 +1,6 @@
 package com.example.wassim.tp2;
 
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
@@ -7,10 +8,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.wassim.tp2.DataStructures.Group;
 import com.example.wassim.tp2.DataStructures.Place;
 import com.example.wassim.tp2.DataStructures.User;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -42,5 +45,6 @@ public void update(){
     }
 
 }
+
     //TODO: backend insert map
 }
--- TP2/app/src/main/java/com/example/wassim/tp2/PlaceScoreAdmin.java
+++ TP2/app/src/main/java/com/example/wassim/tp2/PlaceScoreAdmin.java
@@ -1,18 +1,29 @@
 package com.example.wassim.tp2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.example.wassim.tp2.DataStructures.ContextHolder;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
  * Created by Wassim on 22/03/2017.
  */
 
-public class PlaceScoreAdmin extends AppCompatActivity {
+public class PlaceScoreAdmin extends AppCompatActivity{
 
+    Button buttonAddPlaces;
     RatingBar ratingBar;
+    int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +32,29 @@ public class PlaceScoreAdmin extends AppCompatActivity {
         ContextHolder.setCurrentContext(this);
         ratingBar = (RatingBar) findViewById(R.id.ratingBar);
         //Float ratingNumber = ratingBar.getRating();
+        final Activity a = this;
+
+        buttonAddPlaces = (Button) findViewById(R.id.buttonAddPlaces);
+
+        butListener();
 
         //TODO: backend insert place score, photo...
+    }
+
+    private void butListener() {
+        buttonAddPlaces.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getBaseContext(), MapsActivity.class);
+                startActivity(i);
+
+            }
+        });
+
 
     }
+
+
 }
